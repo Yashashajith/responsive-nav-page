c9400c62bb288dbd2985937308054fd86cbc4b91
Yashashajith <122101895+Yashashajith@users.noreply.github.com>
2023-01-06 16:49:36 +0300

    Add files via upload

---
+++ Javascript tutor/Projects/Practice/Practice1/JSpractice.js
@@ -0,0 +1,61 @@
+function popup() {
+  alert("Hi Dude, I am a alert Box,\n And I am working, \n Thanks for calling me");
+}
+
+
+function dategenrate() {
+  alert(Date());
+}
+
+
+function changetxt() {
+  document.getElementById('changetxt').innerHTML="Hey Dude, now my Name is Morgan, Thanks to change My Name, and I love It, Hurry....!";
+}
+
+function changetxt1() {
+  document.getElementById('changetxt1').innerHTML="Hey Dude, now my Name is Morgan, Thanks to change My Name, and I love It, Hurry....!";
+}
+
+function changetxt2() {
+  document.getElementById('changetxt1').innerHTML="Dude You set My Name back to JACK, I hate it.";
+}
+
+
+function changestyles() {
+  document.getElementById('changestyle').innerHTML="Hey, Button Looks Pretty Good now, Thank You Dude...!"
+  document.getElementById('changebtn').innerHTML="Hurry...! I am Looking Glamarous now, Thank You Dude...!"
+  document.getElementById('changebtn').style.backgroundColor="blue";
+  document.getElementById('changebtn').style.alignItems="center";
+  document.getElementById('changebtn').style.border ="5px solid black";
+  document.getElementById('changebtn').style.boxShadow="13px 23px 33px grey;";
+  document.getElementById('changebtn').style.color="white";
+  document.getElementById('changebtn').style.borderRadius="50px";
+  document.getElementById('changebtn').style.font = "italic bold 13px arial,serif";
+  document.getElementById('changebtn').style.padding = "10px";
+}
+
+function changestyles1() {
+  document.getElementById('changestyle1').innerHTML="Hey, Button Looks Pretty Good now, Thank You Dude...!"
+  document.getElementById('changebtn1').innerHTML="Hurry...! I am Looking Glamarous now, Thank You Dude...!"
+  document.getElementById('changebtn1').style.backgroundColor="blue";
+  document.getElementById('changebtn1').style.alignItems="center";
+  document.getElementById('changebtn1').style.border ="5px solid black";
+  document.getElementById('changebtn1').style.boxShadow="13px 23px 33px grey;";
+  document.getElementById('changebtn1').style.color="white";
+  document.getElementById('changebtn1').style.borderRadius="50px";
+  document.getElementById('changebtn1').style.font = "italic bold 13px arial,serif";
+  document.getElementById('changebtn1').style.padding = "10px";
+}
+
+function disablestyles() {
+  document.getElementById('changestyle1').innerHTML="Due you disabled all the style, you grabbed all  my beauty, this is not talrable.!"
+  document.getElementById('changebtn1').innerHTML="No... ! Please return my beauty back.....!"
+  document.getElementById('changebtn1').style.backgroundColor="white";
+  document.getElementById('changebtn1').style.alignItems="left";
+  document.getElementById('changebtn1').style.border ="1px solid black";
+  document.getElementById('changebtn1').style.boxShadow="none";
+  document.getElementById('changebtn1').style.color="black";
+  document.getElementById('changebtn1').style.borderRadius="0px";
+  document.getElementById('changebtn1').style.font = "0px";
+  document.getElementById('changebtn1').style.padding = "0px";
+}
